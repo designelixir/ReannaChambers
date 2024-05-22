8c868c2771389a6373f28e726f14ce49b35c04e1
Megan Byers <megan@designelixir.studio>
2024-05-22 12:42:17 -0600

    small adjustment

--- scripts/header.js
+++ scripts/header.js
@@ -10,7 +10,7 @@ class Header extends HTMLElement {
           <a href="/ReannaChambers/about" class="nav-link hover engraved" id="About">About</a>
           <a href="/ReannaChambers/photo" class="nav-link hover engraved" id="Photo">Photo</a>
           
-          <h1 class="logo desktop-logo hover engraved" style="font-family: 'medusa', sans-serif; font-size: 36px"/>Reanna Chambers </h1>
+          <h1 class="logo desktop-logo hover engraved" style="font-family: 'medusa', sans-serif; font-size: 36px">Reanna Chambers </h1>
           <a href="/ReannaChambers/production" class="nav-link hover engraved" id="Production">Production</a>
           <a href="/ReannaChambers/video" class="nav-link hover engraved" id="Video">Video</a>
           
