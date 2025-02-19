351501b489869545f2733ca58603a26907214c54
Megan Byers <megan@designelixir.studio>
2025-02-19 01:40:14 -0700

    fix footer icons

--- reanna-chambers/app/components/Footer.tsx
+++ reanna-chambers/app/components/Footer.tsx
@@ -8,16 +8,16 @@ export default function Footer({ dark = false }: FooterProps) {
     <footer className={`basic-padding flex-center-center flex-column ${dark ? "dark-section" : ""}`} style={{ marginTop: "2px" }}>
       <div className="flex-center-center margin-bottom">
         <Link href="mailto:reannachambers@gmail.com" target="_blank" className="flex-center-start no-flex-grow no-link-styling">
-        <span className="icon-email social-icon white-text-glow"></span>
+        <span className={`icon-email social-icon ${dark ? "white-text-glow" : "black-text-glow"}`}></span>
         </Link>
         <Link href="https://instagram.com/reannasea" target="_blank" className="flex-center-start no-flex-grow no-link-styling">
-          <span className="icon-instagram social-icon white-text-glow"></span>
+        <span className={`icon-instagram social-icon ${dark ? "white-text-glow" : "black-text-glow"}`}></span>
         </Link>
         <Link href="https://www.youtube.com/@reannasea" target="_blank" className="flex-center-start no-flex-grow no-link-styling">
-        <span className="icon-youtube social-icon white-text-glow"></span>
+        <span className={`icon-youtube social-icon ${dark ? "white-text-glow" : "black-text-glow"}`}></span>
         </Link>
         <Link href="https://foundation.app/@reanna" target="_blank" className="flex-center-start no-flex-grow no-link-styling">
-        <span className="icon-foundations social-icon white-text-glow"></span>
+        <span className={`icon-foundations social-icon ${dark ? "white-text-glow" : "black-text-glow"}`}></span>
         </Link>
       </div>
       <p style={{fontSize: '14px'}} className={`margin-bottom modern-font ${dark ? "white-text white-text-glow" : "black-text black-text-glow"}`}>Copyright &copy; Reanna Chambers, 2025</p>
